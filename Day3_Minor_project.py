2c6813345a4fdb31ae8c6c2ba039def930de7078
yashturakhia7 <yash.t23pgai@jioinstitute.edu.in>
2022-09-18 21:58:53 +0530

    Day3_Minor_project.py

---
+++ Day3_Minor_project.py
@@ -0,0 +1,10 @@
+#Design a food menu app where a user can select an item out of multiple choices shown to the user. The item selected by the user is shown to the user as the output. 
+
+dishses = int(input("Select you Choice \n 1.Pizza \n 2.Pasta \n 3.Sandwich \n Enter your Choice " ))
+
+if dishses == 1:
+    print("Pizza")
+elif dishses ==2:
+    print("Pasta")
+elif dishses ==3:
+    print("Sandwich")
